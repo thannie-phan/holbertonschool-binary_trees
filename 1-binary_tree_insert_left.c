11b05b2325f093ccce11defec1b7e3d3ff7fe13b
“thannie-phan” <thannie.phan@gmail.com>
2025-08-05 00:40:52 +0000

    task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,10 +13,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *result;
+
 	if (parent == NULL)
 	return (NULL);
 
-	binary_tree_t *result = malloc(sizeof(binary_tree_t));
+	result = malloc(sizeof(binary_tree_t));
 
 	if (result == NULL)
 	return (NULL);
