7b169bb0b8e395049a8e6134b26feb755cc847ea
“thannie-phan” <thannie.phan@gmail.com>
2025-08-05 01:23:08 +0000

    task7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -18,5 +18,5 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, funct);
+	binary_tree_inorder(tree->right, func);
 }
