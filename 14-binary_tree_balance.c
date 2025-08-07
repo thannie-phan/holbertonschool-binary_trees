9599f5b196b05ae7926d15b36a3e932df7a13eb6
“thannie-phan” <thannie.phan@gmail.com>
2025-08-07 00:05:50 +0000

    task14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ *  binary_tree_balance - measure balance factor
+ * @tree: pointer to the root to start traversing
+ *
+ *
+ * Return: -ve, +ve or 0 int to indicate balance factor . 0 if tree is NULL.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height = 0;
+	int right_height = 0;
+
+	if (tree == NULL)
+	return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
