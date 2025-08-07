1b4e4a12d3b9e7381701ac7bc16c4b2daa1d32b5
“thannie-phan” <thannie.phan@gmail.com>
2025-08-07 05:03:57 +0000

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,7 +4,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measure balance factor
+ *  binary_tree_height - measure height of binary tree
+ * @tree: pointer to the root to start traversing
+ *
+ *
+ * Return: height. 0 is tree is NULL.
+ */
+
+int binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height = 0;
+	int right_height = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+	return (left_height + 1);
+	else
+	return (right_height + 1);
+}
+
+
+/**
+ *  binary_tree_balance - measure balance factor
  * @tree: pointer to the root to start traversing
  *
  *
@@ -13,14 +39,14 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0;
-	int right_height = 0;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
 	return (0);
 
-	left_height = 1 + binary_tree_balance(tree->left);
-	right_height = 1 + binary_tree_balance(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
