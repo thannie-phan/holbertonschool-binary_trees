1b2557bf07cb9d8d190da556551039e72809b0e4
“thannie-phan” <thannie.phan@gmail.com>
2025-08-07 05:47:40 +0000

    task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -22,7 +22,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	if ((tree->left != NULL && tree->right == NULL)
+	if ((tree->left != NULL && tree->right == NULL))
 		return (0);
 
 	if (tree->left != NULL && tree->right != NULL)
